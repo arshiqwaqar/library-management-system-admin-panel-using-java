5df7801a651cd3d49198f1fad17b1bc791abc45f
arshiqwaqar <70901591+arshiqwaqar@users.noreply.github.com>
2023-02-23 02:18:15 +0500

    Add files via upload

---
+++ src/JOptionpane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hammad
+ */
+class JOptionpane {
+
+    static void showMessageDialog(Object object, String incorrect_username_or_password) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
